4c8fefc4958c643b70de0b4637695935a2d46117
Elian Doran <doran.adoris.elian@gmail.com>
2021-04-04 20:06:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UwpCompanion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
